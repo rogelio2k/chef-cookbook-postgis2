041e715326d155d02010103e1c71521359172e1a
Rogelio Castillo A <rogelio@rogeliocastillo.com>
2012-09-03 15:14:54 -0300

    renaming files

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,16 +3,16 @@ include_recipe "postgresql::server"
 include_recipe "postgresql::server_debian"
 
 # install support packages
-include_recipe "postgis2::install-support-packages"
+include_recipe "postgis2::install_support_packages"
 
 # add apt repositories
-include_recipe "postgis2::add-apt-repositories"
+include_recipe "postgis2::add_apt_repositories"
 
 # install support packages
-include_recipe "postgis2::install-postgis2"
+include_recipe "postgis2::install_postgis2"
 
 # create db template
-include_recipe "postgis2::create-db-template"
+include_recipe "postgis2::create_db_template"
 
 # initialise db template
-include_recipe "postgis2::initialise-db-template"
+include_recipe "postgis2::initialise_db_template"
