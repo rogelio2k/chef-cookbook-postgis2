f50a6a21b8598605015f71d049a14c11af2f974d
David Watson <davidthewatson@gmail.com>
2012-11-30 11:24:37 -0500

    Update recipes/create_db_template.rb
    This change fixes the issue where "vagrant up" throws an exception because UTF8 does not match the system's LC_CTYPE of LATIN1.

--- recipes/create_db_template.rb
+++ recipes/create_db_template.rb
@@ -1,6 +1,6 @@
 execute :create_postgis_template do
 	user "postgres"
-	command "createdb -E UTF8 #{node[:postgis][:template_name]}"
+	command "createdb -E UTF8 --locale=en_US.utf8 -T template0 #{node[:postgis][:template_name]}"
 	not_if "psql -qAt --list | grep '^#{node[:postgis][:template_name]}\|'", :user => 'postgres'
 	action :run
 end
