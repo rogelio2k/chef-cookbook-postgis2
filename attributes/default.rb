23143e5f7225ebd9b335f2ee4a69c8bd422a179a
Zachary Maillard <sagebrushgis@gmail.com>
2014-05-12 08:48:05 -0600

    Changed PostGIS version attribute to default to the PostgreSQL version number

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default[:postgis][:template_name] = "template_postgis"
 
 #Set the version number for the current version
 #of PostgreSQL
-default[:postgis][:postgresversion] = "9.3"
+default[:postgis][:postgresversion] = node["postgresql"]["version"]
 
 default[:postgis][:package] = "postgresql-#{node[:postgis][:postgresversion]}-postgis-2.1"
 default[:postgis][:packagescripts] = "postgresql-#{node[:postgis][:postgresversion]}-postgis-2.1-scripts"
