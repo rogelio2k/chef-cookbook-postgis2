cac0306e93ed9c80849bb6206b173784d8c40f9d
Zachary Maillard <sagebrushgis@gmail.com>
2014-05-12 08:21:31 -0600

    Added a attribute to specify postgresql version for PostGIS. Working on a way to use the postgresql version number.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
-include_attribute 'postgresql'
+include_attribute "postgresql"
 
 default[:postgis][:template_name] = "template_postgis"
 
 #Set the version number for the current version
 #of PostgreSQL
-default[:postgis][:postgresversion] = default['postgresql']['version']
+default[:postgis][:postgresversion] = "9.3"
 
-default[:postgis][:package] = "postgresql-#{default[:postgis][:postgresversion]}-postgis-2.1"
-default[:postgis][:packagescripts] = "postgresql-#{default[:postgis][:postgresversion]}-postgis-2.1-scripts"
+default[:postgis][:package] = "postgresql-#{node[:postgis][:postgresversion]}-postgis-2.1"
+default[:postgis][:packagescripts] = "postgresql-#{node[:postgis][:postgresversion]}-postgis-2.1-scripts"
--- recipes/install_postgis2.rb
+++ recipes/install_postgis2.rb
@@ -1,8 +1,8 @@
-package default[:postgis][:package] do
+package node[:postgis][:package] do
   action :install
   options "--force-yes" # due the workaround to add new repositories, this option is needed to installed the new packages
 end
 
-package default[:postgis][:packagescripts] do
+package node[:postgis][:packagescripts] do
   action :install
 end
